a086f8b80bca10c7ea078efcd91252e03dc47ed4
Cringy <corbomatteo08@gmail.com>
2026-02-15 23:17:17 +0100

    Readded FormValidationResult type

--- ts/validation/validation_types.ts
+++ ts/validation/validation_types.ts
@@ -1,3 +1,4 @@
+import { RegistrationFormData } from "../types/signup_form_data";
 import { ValidationErrorCode } from "./error_codes";
 
 export interface FieldValidationResult {
@@ -9,3 +10,10 @@ export const validFieldResult: FieldValidationResult = {
     isValid: true,
     errorCode: null,
 } as const;
+
+export interface FormValidationResult {
+    isValid: boolean,
+    fields: {
+        [K in keyof RegistrationFormData]?: FieldValidationResult;
+    }
+}
