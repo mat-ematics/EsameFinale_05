3d07fc34ac3412f44e6e14924ab030a73ff2e496
Cringy <corbomatteo08@gmail.com>
2026-02-26 19:22:16 +0100

    Created Overall Validation Logic and renamed signup to register for consistency

---
+++ ts/register.ts
@@ -0,0 +1,17 @@
+import { validateRegistrationForm } from "./validation/form_validator.js";
+import { extractRegistrationFormData } from "./types/registration_form_data.js";
+import { renderForm } from "./ui/form_rendering.js";
+
+const registrationForm = document.getElementById('formRegister') as HTMLFormElement;
+
+registrationForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const data = extractRegistrationFormData(registrationForm);
+    const result = validateRegistrationForm(data);
+    renderForm(registrationForm, result);
+
+    if (result.isValid) {
+        /* Invio Form */
+    }
+});
